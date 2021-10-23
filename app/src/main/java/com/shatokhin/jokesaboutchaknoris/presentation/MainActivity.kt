f9c2912be994db0f38bb9d0ae5d959512419d896
Artemshatokhin92 <Artemshatokhin92@gmail.com>
2021-10-23 17:33:27 +0300

    create project

---
+++ app/src/main/java/com/shatokhin/jokesaboutchaknoris/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shatokhin.jokesaboutchaknoris.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shatokhin.jokesaboutchaknoris.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
